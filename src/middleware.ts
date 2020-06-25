bad8fb887f9efd377c69d934f794db50642cc658
buuer <hchi404@gmail.com>
2020-06-25 23:45:08 +0800

    add  middleware type

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,25 @@ import { VevConf } from './index.ts'
 import { tap, pReject, pResolve, isFn } from './utils.ts'
 import { vevError } from './errorCode.ts'
 
-export type middleware = <T>(c: VevConf, next: next) => Promise<T>
+export type middleware = <T>(c: VevConf | undefined, next: next) => Promise<T>
 export type middlewareConf = Partial<VevConf> | ((c: VevConf) => VevConf | Promise<VevConf>)
 export type middlewareRes = (c: any) => Promise<any>
 
 type next = (...c: any[]) => Promise<any>
 
-type composeVev = <T>(...mid: middleware[]) => (config?: VevConf, next?: next) => Promise<T>
+export type composeVev = (mid: middleware[]) => <T>(c?: VevConf, next?: next) => Promise<T>
 
-type dispatch = (idx: number, arg: any) => Promise<any>
+type dispatch = <T>(idx: number, arg: any) => Promise<T>
 
-export const composeVev: composeVev = (...mid) => {
-  if (!mid.length || !mid.every(isFn)) throw vevError(0)
+export const middlewareCheck = (midList: middleware[]) => {
+  if (midList.length && !midList.every(isFn)) throw vevError(0)
+}
+
+export const composeVev: composeVev = (mid) => {
+  middlewareCheck(mid)
 
   return (config, finalCall) => {
-    let callIndex = 0
+    let callIndex = -1
 
     const dispatch: dispatch = (idx, arg) => {
       if (arg === undefined) throw vevError(1)
@@ -25,8 +29,8 @@ export const composeVev: composeVev = (...mid) => {
 
       if (!fn) return pResolve(arg)
 
-      if (callIndex !== idx) throw vevError(2)
       callIndex += 1
+      if (callIndex !== idx) throw vevError(2)
 
       try {
         return pResolve(fn(arg, dispatch.bind(null, idx + 1)))
@@ -37,7 +41,7 @@ export const composeVev: composeVev = (...mid) => {
 
     return dispatch(0, config || null).then(
       tap(() => {
-        if (callIndex !== mid.length + 1) {
+        if (callIndex !== mid.length) {
           throw vevError(2)
         }
       })
